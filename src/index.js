8cbf9fe4c5172e68262db80a8966b1c62eeebd51
otsembo <okumu.otsembo@gmail.com>
2022-03-08 14:40:21 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+    const dom = document;
+    //form handler
+    let todoForm = dom.getElementById("create-task-form");
+    todoForm.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        //to-do task
+        let toDO = dom.getElementById('new-task-description').value;
+        if(toDO === ''){
+            alert("You can not have a blank task");
+        }else{
+            let todoList = dom.getElementById('tasks');
+            const liTodo = dom.createElement('li');
+            liTodo.textContent = toDO;
+            todoList.append(liTodo);
+        }
+    })
+
+
 });
